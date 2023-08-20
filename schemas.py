a357fafdf779799d6438f5766dec516b91f6aa54
Håkon Skogland Halvorsen <hakon@alv.no>
2023-08-20 20:52:13 +0200

    details

--- schemas.py
+++ schemas.py
@@ -13,11 +13,19 @@ class PlainResultSchema(Schema):
     event_id = fields.Int(required=True)
     rank = fields.Int(required=True)
     
+class PlainEventResultSchema(Schema):
+    athlete_id = fields.Int(required=True)
+    rank = fields.Int(required=True)
+    
+class PlainAthleteResultSchema(Schema):
+    event_id = fields.Int(required=True)
+    rank = fields.Int(required=True)
+    
 class AthleteSchema(PlainAthleteSchema):
-    results = fields.List(fields.Nested(PlainResultSchema()), dump_only=True)
+    results = fields.List(fields.Nested(PlainAthleteResultSchema()), dump_only=True)
     
 class EventSchema(PlainEventSchema):
-    results = fields.List(fields.Nested(PlainResultSchema()), dump_only=True)
+    results = fields.List(fields.Nested(PlainEventResultSchema()), dump_only=True)
     
 class ResultSchema(PlainResultSchema):
     athlete = fields.Nested(PlainAthleteSchema(), load_only=True)
